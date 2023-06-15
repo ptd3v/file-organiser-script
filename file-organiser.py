217c5b413a849c2b60fa37c6099359bbd8441f82
ptd3v <102877038+ptd3v@users.noreply.github.com>
2023-06-15 16:07:54 +0100

    File Organiser Completed

--- file-organiser.py
+++ file-organiser.py
@@ -1,25 +1,25 @@
-#Import OS for operating system interaction
+#Import OS for operating system interaction.
 import os
-#Import Shell Utilities for high level file operations
+#Import Shell Utilities for high level file operations.
 import shutil
 
 def organize_files(directory):
-    #Assigned folders for different file categories
+    #Assigned folders for different file categories.
     folders = {
         "Excel Spreadsheets": [".xls", ".xlsx", ".csv"],
         "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
         "Text Files": [".txt"],
         "Word Documents": [".doc", ".docx"]
-        #Structure: "Folder Name": [".fileextension"]. Remember to add/remove the ',' as required.
+        #To add more: "Folder Name": [".extension"]. Remember to add/remove the ',' as required.
     }
 
-    #Iterate through each file in the directory
+    #Go through every file in the specifed directory.
     for filename in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, filename)):
-            # Get the file extension
+            #Get the file extension
             extension = os.path.splitext(filename)[1]
 
-            # Check the extension against each category and move the file
+            #Check the extension against each category above and move the file.
             for folder, extensions in folders.items():
                 if extension.lower() in extensions:
                     target_folder = os.path.join(directory, folder)
@@ -27,10 +27,11 @@ def organize_files(directory):
                         os.makedirs(target_folder)
                     shutil.move(os.path.join(directory, filename), os.path.join(target_folder, filename))
 
-# Provide the directory path where the files are located
-directory_path = "C:/Your/Folder/Path"
+#Provide the directory path where the files are located.
+directory_path = "C:/file-organiser-script/test-data"
 
-# Call the function to organize the files
+#Call the function to organize the files.
 organize_files(directory_path)
 
+#A nice little confirmation message for when it's completed.
 print("File organization complete.")
