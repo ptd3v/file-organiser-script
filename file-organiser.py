13df07121277e91e780fef80b0206a3b3f3ec0e6
ptd3v <102877038+ptd3v@users.noreply.github.com>
2023-06-15 15:58:12 +0100

    File Structure Update

--- file-organiser.py
+++ file-organiser.py
@@ -12,3 +12,25 @@ def organize_files(directory):
         "Word Documents": [".doc", ".docx"]
         #Structure: "Folder Name": [".fileextension"]. Remember to add/remove the ',' as required.
     }
+
+    #Iterate through each file in the directory
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            # Get the file extension
+            extension = os.path.splitext(filename)[1]
+
+            # Check the extension against each category and move the file
+            for folder, extensions in folders.items():
+                if extension.lower() in extensions:
+                    target_folder = os.path.join(directory, folder)
+                    if not os.path.exists(target_folder):
+                        os.makedirs(target_folder)
+                    shutil.move(os.path.join(directory, filename), os.path.join(target_folder, filename))
+
+# Provide the directory path where the files are located
+directory_path = "C:/Your/Folder/Path"
+
+# Call the function to organize the files
+organize_files(directory_path)
+
+print("File organization complete.")
